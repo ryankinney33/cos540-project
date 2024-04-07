11ece2ba0e553e35268b553d810a7ea003894cea
Ryan Kinney <rkinney796@gmail.com>
2024-04-07 17:57:09 -0400

    Define Philly's macros

--- client.c
+++ client.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#include <pthread.h>
+
+#define SRV_TCP_A   "127.0.0.1"
+#define SRV_TCP_P   27020
+#define CLI_UDP_A   "0.0.0.0"
+#define CLI_UDP_P   25567
+
+/* These are extraneous... */
+#define CLI_TCP_A   "0.0.0.0"
+#define CLI_TCP_P   27021
+
 int main() {
 	printf("Client!\n");
 	return 0;
--- server.c
+++ server.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+#include <pthread.h>
+
+#define SRV_TCP_A   "0.0.0.0"
+#define SRV_TCP_P   27020
+
+#define CLI_UDP_A   "127.0.0.1"
+
+/* These are extraneous... */
+#define CLI_UDP_P   25567
+#define CLI_TCP_A   "0.0.0.0"
+#define CLI_TCP_P   27021
+
 int main() {
 	printf("Server!\n");
 	return 0;
